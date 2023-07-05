7a953d15699a21eb8bffae70ce9e0037d9acbe3a
shalom2552 <shalom2552@gmail.com>
2023-07-05 15:36:19 +0300

    added ui to DashboardFragment

--- app/src/main/java/com/example/tutorial6/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/tutorial6/ui/dashboard/DashboardFragment.java
@@ -22,11 +22,8 @@ public class DashboardFragment extends Fragment {
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
     }
 
     @Override
